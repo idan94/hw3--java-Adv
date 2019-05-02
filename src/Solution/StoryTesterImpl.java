0c207a6d951b24d1ce80a31bdcab65531224f096
IdanCohen <36813204+idan94@users.noreply.github.com>
2019-05-02 12:58:19 +0300

    Initial commit

---
+++ src/Solution/StoryTesterImpl.java
@@ -0,0 +1,15 @@
+package Solution;
+
+import Provided.StoryTester;
+
+public class StoryTesterImpl implements StoryTester {
+    @Override
+    public void testOnInheritanceTree(String story, Class<?> testClass) throws Exception {
+        //TODO: Implement
+    }
+
+    @Override
+    public void testOnNestedClasses(String story, Class<?> testClass) throws Exception {
+        //TODO: Implement
+    }
+}

d5f55aa48d448df0a169ded3840c0caee5821c65
IdanCohen <36813204+idan94@users.noreply.github.com>
2019-05-06 11:07:30 +0300

    Added Files

---
+++ src/Solution/StoryTestExceptionImpl.java
@@ -0,0 +1,23 @@
+package Solution;
+
+import Provided.StoryTestException;
+
+import java.util.List;
+
+public class StoryTestExceptionImpl extends StoryTestException {
+    public String getSentance() {
+        return null;
+    }
+
+    public List<String> getStoryExpected() {
+        return null;
+    }
+
+    public List<String> getTestResult() {
+        return null;
+    }
+
+    public int getNumFail() {
+        return 0;
+    }
+}
